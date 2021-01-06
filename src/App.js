d570a31769a72bd4f53458391c96545cb8b9cc7a
hsj <tohsj0806@qq.com>
2021-01-06 11:03:24 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+class Square extends React.Component {
+ 
+  render() {
+    return (
+      <button onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Borad extends React.Component {
+
+  constructor(props){
+    super(props)
+    console.log(props);
+  }
+
+  state = { title: 'hello2' }
+
+  renderSquare = (i)=> {
+    return <Square  value={i} onClick={this.showThis}/>
+  }
+
+  showThis = (e) => {
+    console.log(this);
+    alert(e)
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ fontSize: '20px' }}>{this.props.title}</div>
+        <div>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const o = {title:'hello',subtitle:' world'}
+
+  return <Borad {...o} />
 }
 
 export default App;
